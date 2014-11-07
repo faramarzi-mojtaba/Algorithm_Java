2b3e6bdc609880f4afd9d10c72d76d5f95874017
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-11-06 20:31:13 -0500

    Prime number algorithm was added

---
+++ Algorithm_Java/src/algorithms/simples/Prime.java
@@ -0,0 +1,28 @@
+package algorithms.simples;
+
+import java.util.Scanner;
+
+public class Prime {
+	public static void main(String[] args) {
+		try(Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Please Enter Your Number :");
+			int number = scanner.nextInt();
+			System.out.println("Your Number Is Prime Number : " + isPrime(number));
+		} catch (Exception e) {
+			System.out.println("Prime --> isPrime " + e.getMessage());
+		}
+		
+	}
+	public static boolean isPrime(int number){
+		try {
+			for(int i = 0; i * i < number; i++){
+				if(number % i == 0)
+					return false;
+			}
+		} catch (Exception e) {
+			System.out.println("Prime --> isPrime " + e.getMessage());
+		}
+		return true;
+	}
+
+}
